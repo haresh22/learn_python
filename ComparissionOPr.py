e4d9e11b26625dd15206fabde7b0ba0da6754134
Haresh Singh <harishrsingh22@gmail.com>
2020-04-18 15:28:51 +0530

    compa ver 1.0

---
+++ ComparissionOPr.py
@@ -0,0 +1,40 @@
+# Comparrision data Type 
+# < Less Than   
+# >  Greater Than 
+# < Less than or Equal to
+# >= Greater than or Equal to 
+# == Equal to 
+# != NOT Equal to 
+##################################################
+a = 10 
+b = 4 
+value = (a > b)
+print(value)
+##################################################
+a = 10 
+b = 4 
+value = (a < b)
+print(value)
+#################################################
+a = 10 
+b = 4 
+value = (a >= b)
+print(value)
+#################################################
+a = 10
+b = 10 
+value = (a == b)
+print(value)
+##################################################
+a = 10
+b =  5
+value = (a != b)
+print(value)
+#####################################################
+
+
+
+
+
+
+
