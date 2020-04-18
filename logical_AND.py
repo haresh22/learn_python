a76d4c092e032b96c36324425eccd3af2cc78824
Haresh Singh <harishrsingh22@gmail.com>
2020-04-18 17:43:49 +0530

    expression added ver 1.1

--- logical_AND.py
+++ logical_AND.py
@@ -32,3 +32,21 @@ c = 2
 d = 10
 value = (a > b) and (d > c)
 print(value)
+########## Expression ############
+# True and Expression is "EXPRESSION " What ever is your last expression 
+# False and Expression is always "FALSE" Its not depend on your last EXPRESSION
+a = 10
+b = 20 
+c = 1000
+d = 100
+e = 500
+f = 12121
+value = (a<b and c and d and e and f)
+print(value)
+#################################
+a = 100
+b = 10
+c = 100000
+d = 1212
+value = (a<b and c and d)
+print(value)
