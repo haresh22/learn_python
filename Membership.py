aea82dad439b7860189fca1415599344d3f968af
Haresh Singh <harishrsingh22@gmail.com>
2020-04-18 22:43:30 +0530

    in and not in ver 1.0

---
+++ Membership.py
@@ -0,0 +1,13 @@
+# Membership IN and NOT IN , Find the word in such file.
+#  
+###  IN   #### if word find you will get the TRUE in result
+a = "My Name is Haresh Singh and i love to hack"
+print("hack" in a)
+#######################################################
+b = "983129810241 109241041 104i1-24i-12 9820953085 kahuasfk Haresh 10.34 "
+print("9820953085" in b)
+
+### NOT IN #### If word find you will get FALSE in result 
+
+a = "Haresh is a hacker"
+print("Haresh" not in a)
