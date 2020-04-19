d200c2eb3235645e6312b2b9800e7d44570aa17c
Haresh Singh <harishrsingh22@gmail.com>
2020-04-19 13:00:46 +0530

    concinate ver 1.0

---
+++ string_concinate.py
@@ -0,0 +1,30 @@
+###### 2 String join or PLUS it means "CONCINATE" ###############
+
+a = "Haresh is Hacker "
+b = "and now he is learing PYTHON "
+c = (a+b)
+print(c)
+####################################################
+
+a = "Haresh is \"Hacker\" "
+b = "and now he is learning \"PYTHON\" "
+c = (a+b)
+print(c)
+####################################################
+
+a = "Haresh is number \'1\' \"HACKER\" "
+b = "in our \"MUMBAI\" "
+c = (a+b)
+print(c)
+
+
+
+
+
+
+
+
+
+
+
+
