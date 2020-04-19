bdc7fa77fbd95dbf71eadc6fd719c4e749a5dcca
Haresh Singh <harishrsingh22@gmail.com>
2020-04-19 12:32:13 +0530

    float to intejar ver 1.0

---
+++ float_to_intejar.py
@@ -0,0 +1,17 @@
+###  FLOAT to INTEJAR # in this you are loosing your data ( . )
+
+a = 9.67
+b = 100
+c = (int(a+b))
+print(c)
+##############################################
+a = 8.67
+b = 44.78
+c = (int(a+b))
+print(c)
+##############################################
+
+a = 300
+b = 192.76
+c = (int(a+b))
+print(c)
