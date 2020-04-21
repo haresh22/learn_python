6c3d037cf12676d97c0d793de35a862f5dbd0482
Haresh Singh <harishrsingh22@gmail.com>
2020-04-22 02:11:54 +0530

    If elif ver 1.0

---
+++ if_elif.py
@@ -0,0 +1,15 @@
+############# IF ELIF and ELSE ###############
+
+a = int(input("Type any number "))
+b = int(input("Type any number "))
+
+if (a>b):
+    print("A is greatern than B ")
+    
+elif (b>a):
+    print("B is Greater than A ")
+
+elif (a==b):
+    print("A and B are Equal ")
+
+        
