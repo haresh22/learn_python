63c93fa7b271497757c91191d1ff22cbf0345e0f
Haresh Singh <harishrsingh22@gmail.com>
2020-04-22 01:40:15 +0530

    if else test ver 1.0

---
+++ if_else.py
@@ -0,0 +1,8 @@
+########################  IF else #########################
+a=10
+b=20
+if (a>b):
+    print("Higher wins") 
+
+else:
+    print("Smaller Wins")
---
+++ test1.py
@@ -0,0 +1,25 @@
+##############  TEST #############
+a = int(input("Type any number : "))
+b = int(input("Type any number : "))
+c = int(input("Type any number : "))
+d = int(input("Type any mumber : "))
+
+if (a>b):
+    print("A is Greater than B ")
+else:
+    print("B is not Greater than A ")    
+    if (b>c):
+            print("B is Greater than C ")
+    
+    else:
+            print("C is Smaller than B" )    
+    
+    if (c<d):
+        print("C is Smaller than D ")
+    
+    else:
+        print("D is Greater than C ")
+
+
+        
+    
