12b722df9cc24bf3d9ca058e714fdfa196272234
Haresh Singh <harishrsingh22@gmail.com>
2020-04-21 21:10:31 +0530

    if ver 1.0

---
+++ if.py
@@ -0,0 +1,10 @@
+
+if(10>5):
+    a = int(input("Type number " ))
+    b = int(input("Type number " ))
+    print(a+b)
+    
+    if(10>2):
+        a = int(input("Type any Number "))
+        b = int(input("Tpye any Number ")) 
+        print(a*b)   
---
+++ input.py
@@ -0,0 +1,7 @@
+############### USER INPUT ####################
+name = input("Please enter your Name : ")
+print("Your Name is name", name )
+age = int(input("Please enter your age : "))
+print("Your age is", age )
+mobile_no = int(input("Please enter your Mobile No : "))
+print("Your Mobile no is ", mobile_no)
