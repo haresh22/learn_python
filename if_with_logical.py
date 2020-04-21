877250fbddb8577f39437e7aa8bfc8c65ef5f858
Haresh Singh <harishrsingh22@gmail.com>
2020-04-21 22:13:14 +0530

    if with logical ver 1.0

--- if_with_logical.py
+++ if_with_logical.py
@@ -1,1 +1,14 @@
+############# IF WITH lOGICAL "and" ###########
+if (10>4) and (10>3):
+    a = input("Enter your Name : ")
+    b = int(input("Enter your age : "))
+    print("Your name is ", a, "and your age is", b,)
 
+###########################################################    
+if (100>20) and (200>50):
+    a = input("Enter your Name : ")
+    b = int(input("Enter your Age : "))
+    if (20<40):
+        c = input("Enter your City : ")
+        d = int(input("Enter your Mobile No : "))
+    print("My Name is", a, "My Age is", b, "My City Name is", c, "My Mobile no is" ,d)
