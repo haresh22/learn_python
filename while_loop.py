52f0954fa8fb2aff0574fe2a53e6c873e7a3d174
Haresh Singh <harishrsingh22@gmail.com>
2020-04-22 10:00:13 +0530

    while loop ver 1.0

---
+++ while_loop.py
@@ -0,0 +1,15 @@
+############# WHILE LOOP ################
+
+a = 1 
+while(a<=10):
+    print(a)
+    a+=1
+print("Eat Sleep Code ")
+
+##########################################
+
+z = 20
+while(z>=1):
+    print(z)
+    z-=1
+print("Eat Sleep Code ")
