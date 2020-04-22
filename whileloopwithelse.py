38919bfdefd6222ea52f4b4efe519b6b0defa457
Haresh Singh <harishrsingh22@gmail.com>
2020-04-22 11:19:41 +0530

    whileloopelse ver 1.0

---
+++ whileloopwithelse.py
@@ -0,0 +1,18 @@
+##############  WHILE LOOP with ELSE #######################
+
+a = 1
+while a<=10:
+    print(a)
+    a+=1
+else:
+    print("Else is Excucated")
+print("Eat Sleep Code ")
+
+######### Direct ELSE " Means While fasle "
+b = 20
+while b<=20:
+    print(b)
+    b+=1
+else:
+    print("Else is Best")
+print("Eat Sleep Code ")    
