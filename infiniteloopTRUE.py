b593403f4b19bb7898325c6107f732507d3b9eb1
Haresh Singh <harishrsingh22@gmail.com>
2020-04-23 22:36:39 +0530

    menu ver 1.0

---
+++ infiniteloopTRUE.py
@@ -0,0 +1,9 @@
+
+a = 0
+while True:
+    a+=1
+    print(a)
+    if(a==10):
+        break
+
+
---
+++ pentaafood.py
@@ -0,0 +1,19 @@
+######## Pentaafood ############
+
+##### Item Name #######  Particular ############
+
+burger = "Veg Burger "
+pizza = "Margherita Pizza "
+sandwich = "Veg Sandwich "
+cold_drink = "Coke"
+tea = "kullhad Chai"
+###############################################
+########### Price ########################
+burger_price = int(50)
+pizza_price = int(100)
+sandwich_price = int(30)
+cold_drink_price = int(20)
+tea_price = int(10)
+###############################################
+
+
