5a730d08d96f5e84cc4116de067d323276f94c29
Haresh Singh <harishrsingh22@gmail.com>
2020-04-23 23:49:11 +0530

    ver 1.0

--- pentaafood.py
+++ pentaafood.py
@@ -8,7 +8,7 @@ sandwich = "Veg Sandwich "
 cold_drink = "Coke"
 tea = "kullhad Chai"
 ###############################################
-########### Price ########################
+########### Price ########################s
 burger_price = int(50)
 pizza_price = int(100)
 sandwich_price = int(30)
@@ -16,4 +16,7 @@ cold_drink_price = int(20)
 tea_price = int(10)
 ###############################################
 
-
+New_Custumer = (input("Enter Custumber Name : "))
+New_Custumer = int(input("Enter Custumer Mobile No :" ))
+input(burger)
+int(input(burger_price))
