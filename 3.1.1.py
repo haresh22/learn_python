1130fd06483d1818ab2523e8d59e375d38dc6da6
Haresh Singh <harishrsingh22@gmail.com>
2020-04-27 01:12:53 +0530

    1.0

---
+++ 3.1.1.py
@@ -0,0 +1,22 @@
+############### IF ad Else #######################
+input("Name of Stdent : ")
+input("Standard : ")
+int(input("Enter your Roll No : "))
+
+a = int(input("Enter the English Marks : "))
+if (a>=35):
+    print("You are Pass ")
+else:
+    print("You are Fail ")    
+b = int(input("Enter The Maths Marks :"))
+if (b>=35):
+    print("You are Pass ")
+else:
+    print("You are Fail ")    
+c = int(input("Enter the Maths Marks : "))
+if c>=35:
+    print("You are Pass")
+else:
+    print("You are Fail ")
+
+print("Total Marks = ", a+b+c // 300 * 100)
