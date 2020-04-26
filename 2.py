a2e2acf1a4be5df27982acef863c6de5b7b879e4
Haresh Singh <harishrsingh22@gmail.com>
2020-04-26 16:40:44 +0530

    call

---
+++ 2.py
@@ -0,0 +1,7 @@
+#################  IF ############
+input("Full Name : ")
+a = int(input("Dial Call in 1 Day is : "))
+if(a>=200):
+    print("Eligeble for Salary ")
+else:
+    print("Not Eligeble for Salary ")    
