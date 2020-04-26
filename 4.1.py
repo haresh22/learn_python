22e6971f72366ba28b3ed4dfccd4f92407d2769c
Haresh Singh <harishrsingh22@gmail.com>
2020-04-26 21:30:51 +0530

    1.2

--- 4.1.py
+++ 4.1.py
@@ -1,3 +1,5 @@
+############### Logical "AND and EXPRESSION" ########################
+
 a = int(input("Enter number between 1 to 10 : "))
 b = int(input("Enter number between 11 to 20 : ")) 
 c = int(input("Enetr number between 21 to 30 : "))
