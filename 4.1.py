d22707ee0476d23f9fb56e679d7537164b320b83
Haresh Singh <harishrsingh22@gmail.com>
2020-04-26 20:59:32 +0530

    ver 1.1

---
+++ 4.1.py
@@ -0,0 +1,7 @@
+a = int(input("Enter number between 1 to 10 : "))
+b = int(input("Enter number between 11 to 20 : ")) 
+c = int(input("Enetr number between 21 to 30 : "))
+
+print(a<b and b>a)
+print(b>a and b<c)
+print(c>b and c>a)
