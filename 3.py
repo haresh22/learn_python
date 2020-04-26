ccd544c43a8cffb5f26f6e4d0847cdb70653f088
Haresh Singh <harishrsingh22@gmail.com>
2020-04-26 20:04:46 +0530

    ver 1.0

---
+++ 3.py
@@ -0,0 +1,15 @@
+###################################### IF and Else ####################################
+a = 5 
+b = 10 
+c = 15 
+d = 20 
+
+if(a<b):
+    print("B is Greater than A ")
+    if(b>a):
+        print("A is Smaller than B ")
+        if(d>c):
+            print("D is Greater than C ")
+            if(c<d):
+                print("C is Smaller than D")
+print("Eat Sleep Code and Repeat ")                
