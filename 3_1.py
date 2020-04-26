6f88920e4f7d001b5db0085c7edaaa19ef9532f8
Haresh Singh <harishrsingh22@gmail.com>
2020-04-26 20:25:01 +0530

    if ver 1.1

---
+++ 3_1.py
@@ -0,0 +1,19 @@
+############ If With user Input##############
+
+a = int(input("Enter any number between 1 to 10 : "))
+b = int(input("Enter any number between 11 to 20 :"))
+c = int(input("Enter any numner between 21 to 30 :"))
+d = int(input("Enter any number between 31 to 40 : "))
+
+if (a<b):
+    print("A is Smaller than B")
+    if (a<b):
+        print("B is Greater Than A ")
+        if (c<d):
+            print("C is Smaller than D")
+            if (d>c):
+                print("D is Gretar than C")
+print("rest of the Code ")                
+            
+        
+    
