ab0cbbe2340c82e3d348dfb3961ec0d97d63a5e5
Haresh Singh <harishrsingh22@gmail.com>
2020-04-27 12:06:53 +0530

    c

--- 4.2.2.py
+++ 4.2.2.py
@@ -4,13 +4,13 @@ a = 5
 b = 3 
 c = 2 
 if(b>a):
-    print("B is Smaller than A ")
+    print("B is Smaller than A ") # False 
 elif (c>a):
-    print("C is Smaller Than A ")
+    print("C is Smaller Than A ") # False 
 elif (b>c):
-    print("B is Greater than C ")    
+    print("B is Greater than C ") # True 
 elif (a<c):
-    print("A is Greater than C ")
+    print("A is Greater than C ") # False  
        
 print("Rest of the Code ")    
             
