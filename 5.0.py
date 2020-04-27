e9cb7d20e7faa001fecc57216bc816ab9321e9c6
Haresh Singh <harishrsingh22@gmail.com>
2020-04-27 13:13:34 +0530

    ver 1.0

---
+++ 5.0.py
@@ -0,0 +1,14 @@
+################### While Loop #################
+
+a = 1
+b = 10
+while (a<=10):
+    print(a)
+    a+=1
+print("first Scprt Over ")
+    
+while (b<=100):
+        print(b)
+        b+=10
+print("Second Scprit Over ") 
+       
