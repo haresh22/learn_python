67d849ca3f911618bef28b718963856d681cc33f
Haresh Singh <harishrsingh22@gmail.com>
2020-04-27 13:21:08 +0530

    ver 1.1

---
+++ 5.1.py
@@ -0,0 +1,16 @@
+################ While Loop with Else #############################
+
+a = 1
+while (a<=20):
+    print(a)
+    a+=1
+else:
+    print("Code Successfully Executed ")
+        
+b = 10 
+while (b<=100):
+    print(b)
+    b+=10
+    print("While Lopp Code Sucessfull Executed ")
+else:
+    print("Else Part Executed NOW Emjoy :)")        
